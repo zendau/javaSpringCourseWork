8264357f7a2c02906a5708ec9d44858a2ff79eee
Zendau <cowalyow2012@gmail.com>
2021-10-18 00:07:24 +0300

    init spring server

---
+++ server/src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping
+public class MainController {
+
+    private Connection connection;
+
+    public MainController() throws SQLException {
+
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javadb", "root", "");
+
+    }
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+
+    @PostMapping
+    public ArrayList testPost(@RequestBody Users test) throws SQLException {
+
+        Statement statement = connection.createStatement();
+        String SQL = "SELECT * FROM goods";
+        ResultSet resultSet =  statement.executeQuery(SQL);
+
+        ArrayList test2 ;
+
+        ArrayList test3 = new ArrayList<>();
+
+        while (resultSet.next()) {
+
+            test2 = new ArrayList<>();
+
+            test2.add(resultSet.getInt("id"));
+            test2.add(resultSet.getString("name"));
+            test2.add(resultSet.getString("category"));
+            test2.add(resultSet.getString("image"));
+            test2.add(resultSet.getString("description"));
+
+            test3.add(test2);
+        }
+
+        System.out.println(test.getId());
+        return test3;
+    }
+
+}
